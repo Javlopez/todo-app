d1571b3a1ff286812b9b08faa734cd4331321ef8
Javier Lopez Lopez <sjavierlopez@gmail.com>
2019-05-16 21:30:57 -0500

    Add props and styles

---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <div style={itemStyle} >
+        <p>{ this.props.todo.title }</p>
+      </div>
+    )
+  }
+}
+
+const itemStyle = {
+    backgroundColor: "#f4f4f4"
+}
+
+//propTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem;
